5896a85aa7da56ad2fad1d640b8cd14f20e9fec1
amyecampbell <campbela12@chop.edu>
2026-08-14 12:00:37 -0400

    mash info relative path

--- Assembly_QC/MashInfo.sh
+++ Assembly_QC/MashInfo.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Amy Campbell
-# Mash search on 99 ST105 isolates
+# Mash search 
 
 source /home/campbela12/miniforge3/bin/activate ST105Env
 
-mash info -t /scr1/users/campbela12/downloadedDBs/RefSeqSketchesDefaults.msh > /scr1/users/campbela12/downloadedDBs/Mashinspected.tab
+mash info -t RefSeqSketchesDefaults.msh > Mashinspected.tab
 
 

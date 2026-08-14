6cd47de7c6df28f7ed93381155c2e68e1d15ca6f
amyecampbell <campbela12@chop.edu>
2026-08-14 12:52:05 -0400

    update paths

--- MolecularClockTree/PastML_Run_Full_MolecClock.sh
+++ MolecularClockTree/PastML_Run_Full_MolecClock.sh
@@ -2,4 +2,4 @@
 source /home/campbela12/miniforge3/bin/activate pastmlenv
 # PastML on the full SAE tree by country 
 
-pastml --tree strictClock100MillTree_StrippedDown.newick --data ./DataArchive/SAEcountries.csv --columns Location --html_compressed sae_fulltreeMolecClock.html --data_sep , --html sae_fulltree_fullMolecClock.html --work_dir fullSAEpastMLoutputMolecClock
+pastml --tree ../DataArchive/Figure\ 3/strictClock100MillTree_StrippedDown.newick --data ./DataArchive/SAEcountries.csv --columns Location --html_compressed sae_fulltreeMolecClock.html --data_sep , --html sae_fulltree_fullMolecClock.html --work_dir ../DataArchive/Figure\ 3/fullSAEpastMLoutputMolecClock
